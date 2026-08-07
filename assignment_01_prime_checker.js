33ddf4dfce8e741cbcf1bf623a779c2c440bd3fd
nusurayakini58 <nusurayakini58@gmail.com>
2026-08-07 20:44:55 +0000

    completed assignment 1

--- assignment_01_prime_checker.js
+++ assignment_01_prime_checker.js
@@ -43,3 +43,33 @@
 // =============================================================================
 
 
+function isPrime(number) {
+    // Numbers less than 2 are not prime
+    if (number < 2) {
+        return false;
+    }
+
+    // Check for divisors
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// -----------------------------------------------------------------------------
+// Main function
+// -----------------------------------------------------------------------------
+function main() {
+    const number = readlineSync.questionInt("Enter a number: ");
+
+    if (isPrime(number)) {
+        console.log(number + " is a prime number.");
+    } else {
+        console.log(number + " is NOT a prime number.");
+    }
+}
+
+main();
